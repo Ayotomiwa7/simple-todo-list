795901c8851d24a0b23f2538a0a94a62dfff6529
Ayotomiwa <ayofesnglimited@gmail.com>
2024-05-01 17:29:30 +0100

    new commit

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('hello')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "home page"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
